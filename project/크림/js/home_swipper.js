b79e0d5bb8ec3c7e287d21c29e403de76b7761f2
victoai <victoai999@gmail.com>
2024-07-24 18:00:24 +0900

    0724

---
+++ project/크림/js/home_swipper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
